df910025fc6022820323e8a5e16be31bdb8b314c
Santhanam Elumalai <selumalai@opentable.com>
2022-09-19 21:10:35 -0600

    scss auto watch

--- src/client/components/blogPages/index.js
+++ src/client/components/blogPages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.scss';
 
-const BlogPages = () => <span className="blogPages">Hello from child</span>;
+const BlogPages = () => <span className="blogPages">Hello from child change</span>;
 
 export default BlogPages;
