dd4abfb8750166d8b2e343c1a92a65a66640fe0a
Santhanam Elumalai <selumalai@opentable.com>
2022-09-19 21:00:00 -0600

    page routing and handler

--- src/server/routeInfo.js
+++ src/server/routeInfo.js
@@ -3,6 +3,10 @@ const ROUTE_INFO = {
         pageTitle: 'List View',
         scriptName: 'listView',
     },
+    '/blogPages': {
+        pageTitle: 'Blog pages',
+        scriptName: 'blogPages',
+    },
 };
 
 export default ROUTE_INFO;
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import BlogPageController from '../controllers/blogPagesController';
 import ListViewController from '../controllers/listViewController';
 
 const router = express.Router();
+router.get('/blogPages', BlogPageController);
 router.get('/listView', ListViewController);
 router.get('/', (req, res) => {
     res.send({});
