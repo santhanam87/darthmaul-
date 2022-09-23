45ea1591d928f16b0188d3484c1dfaf5d8aa99e6
Santhanam Elumalai <selumalai@opentable.com>
2022-09-22 22:01:25 -0600

    post detail page

--- src/client/components/blogPages/index.js
+++ src/client/components/blogPages/index.js
@@ -5,7 +5,7 @@ const BlogPages = () => (
     <>
         <div className="topBand"></div>
         <header className="pageHeader">
-            <h1 className="authorName">SANTHANAM ELUMALAI</h1>
+            <h1 className="siteTitle">SANTHANAM BLOG</h1>
             <nav className="primaryNavigation">
                 <ul>
                     <li>
@@ -26,8 +26,64 @@ const BlogPages = () => (
             </blockquote>
             <main role="main" className="pageBody">
                 <div className="pageArticles">
-                    <h2 className="sectionTitle">Recent Articles</h2>
+                    {/* <h2 className="sectionTitle">Recent Articles</h2> */}
                     <article className="articleContainer">
+                        <div className="articleMetaInfo">
+                            <div className="authorInfo">
+                                <h1 className="authorName">
+                                    <a href="#">Santhanam Elumalai</a>
+                                </h1>
+                                <time dateTime="2018-07-07">July 7th 2018</time>
+                            </div>
+                            <nav className="tagList">
+                                <ul>
+                                    <li>
+                                        <a href="#">UX</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Design</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Guides</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <h3 className="articleHeader">
+                            <a href="#">Lorem Ipsum has been the industry's standard.</a>
+                        </h3>
+                        <div className="articleContent">
+                            <p>
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                                unknown printer took a galley of type and scrambled it to make a type specimen book. It
+                                has survived not only five centuries,
+                            </p>
+                            <h3>Lorem Ipsum</h3>
+                            <p>
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                                unknown printer took a galley of type and scrambled it to make a type specimen book. It
+                                has survived not only five centuries,
+                            </p>
+                        </div>
+                        <div className="articleFooter">
+                            <time dateTime="2018-07-07">July 7th 2018</time>
+                            <nav className="tagNavigation">
+                                <ul>
+                                    <li>
+                                        <a href="#">UX</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Design</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Guides</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </article>
+
+                    {/* <article className="articleContainer">
                         <h3 className="articleHeader">
                             <a href="#">Lorem Ipsum has been the industry's standard.</a>
                         </h3>
@@ -53,6 +109,7 @@ const BlogPages = () => (
                             </nav>
                         </div>
                     </article>
+
                     <article className="articleContainer">
                         <h3 className="articleHeader">
                             <a href="#">Lorem Ipsum has been the industry's standard.</a>
@@ -78,33 +135,7 @@ const BlogPages = () => (
                                 </ul>
                             </nav>
                         </div>
-                    </article>
-                    <article className="articleContainer">
-                        <h3 className="articleHeader">
-                            <a href="#">Lorem Ipsum has been the industry's standard.</a>
-                        </h3>
-                        <p className="articleContent">
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                            unknown printer took a galley of type and scrambled it to make a type specimen book. It has
-                            survived not only five centuries,
-                        </p>
-                        <div className="articleFooter">
-                            <time dateTime="2018-07-07">July 7th 2018</time>
-                            <nav className="tagNavigation">
-                                <ul>
-                                    <li>
-                                        <a href="#">UX</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Design</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Guides</a>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
-                    </article>
+                    </article> */}
                 </div>
                 <div className="pageCategories">
                     <h2 className="sectionTitle">Categories</h2>
