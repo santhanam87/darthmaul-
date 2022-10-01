c0110e9559d71841a4762c8f23294df545556169
Santhanam Elumalai <selumalai@opentable.com>
2022-10-01 17:12:58 -0600

    blog pages update

--- src/client/components/blogPages/index.js
+++ src/client/components/blogPages/index.js
@@ -5,7 +5,9 @@ const BlogPages = () => (
     <>
         <div className="topBand"></div>
         <header className="pageHeader">
-            <h1 className="siteTitle">SANTHANAM BLOG</h1>
+            <h1 className="siteTitle">
+                <a href="/">SANTHANAM TECH BLOG</a>
+            </h1>
             <nav className="primaryNavigation">
                 <ul>
                     <li>
@@ -21,13 +23,11 @@ const BlogPages = () => (
             </nav>
         </header>
         <div className="pageContainer">
-            <blockquote className="quotedText">
-                <p>TODO: Need to add some valuable information for the control-group.</p>
-            </blockquote>
             <main role="main" className="pageBody">
                 <div className="pageArticles">
-                    <h2 className="sectionTitle">Recent Articles</h2>
                     <article className="articleContainer">
+                        <h3 className="articleHeader">Lorem Ipsum has been the industry's standard.</h3>
+
                         <div className="articleMetaInfo">
                             <div className="authorInfo">
                                 <h1 className="authorName">
@@ -49,15 +49,25 @@ const BlogPages = () => (
                                 </ul>
                             </nav>
                         </div>
-                        <h3 className="articleHeader">
-                            <a href="#">Lorem Ipsum has been the industry's standard.</a>
-                        </h3>
                         <div className="articleContent">
                             <p>
-                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
-                                unknown printer took a galley of type and scrambled it to make a type specimen book. It
-                                has survived not only five centuries,
+                                <code>Lorem Ipsum has been the</code> industry's standard dummy text ever since the
+                                1500s, when an unknown printer took a galley of type and scrambled it to make a type
+                                specimen book. It has survived not only five centuries,
                             </p>
+                            <h2>
+                                <a href="#">Header with anchor</a>
+                            </h2>
+                            <ul>
+                                <li>Lorem Ipsum has been</li>
+                                <li>Ipsum has been</li>
+                                <li>Lorem has been</li>
+                            </ul>
+                            <ol>
+                                <li>Lorem Ipsum has been</li>
+                                <li>Ipsum has been</li>
+                                <li>Lorem has been</li>
+                            </ol>
                             <h3>Lorem Ipsum</h3>
                             <p>
                                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
@@ -124,6 +134,19 @@ const BlogPages = () => (
                             </nav>
                         </div>
                     </article>
+                    <section className="postPagination">
+                        <nav>
+                            <div className="prev"></div>
+                            <a href="#" className="page-numbers">
+                                1
+                            </a>
+                            <span className="page-numbers current">2</span>
+                            <a href="#" className="page-numbers">
+                                3
+                            </a>
+                            <div className="next"></div>
+                        </nav>
+                    </section>
                 </div>
                 <div className="pageCategories">
                     <h2 className="sectionTitle">Categories</h2>
@@ -140,6 +163,25 @@ const BlogPages = () => (
                             </li>
                         </ul>
                     </nav>
+                    <h2 className="sectionTitle">Search</h2>
+                    <section className="searchSection">
+                        <form>
+                            <section className="formSection">
+                                <input type="text" placeholder="Search" />
+
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="32"
+                                    height="32"
+                                    fill="currentColor"
+                                    className="bi bi-search"
+                                    viewBox="0 0 16 16"
+                                >
+                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                                </svg>
+                            </section>
+                        </form>
+                    </section>
                 </div>
             </main>
         </div>
